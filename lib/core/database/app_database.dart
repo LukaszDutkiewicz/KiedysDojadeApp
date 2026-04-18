5ab775320b74cdb267a1061c6f6c5e73cbc0f735
LukaszDutkiewicz <lukdut@st.amu.edu.pl>
2026-04-18 21:22:32 +0200

    Add folder sturucture

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -5,7 +5,13 @@ import 'connection/native.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [])
+class FavoriteStops  extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get stopId => text()();
+  TextColumn get stopName => text()();
+}
+
+@DriftDatabase(tables: [FavoriteStops])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(openConnection());
 
